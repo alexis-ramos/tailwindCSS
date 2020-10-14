3a5ee81dee7576e78fa7fd47a55773aa329b75b7
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-10-14 17:02:49 -0500

    make a header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary':'#f3c614',
+        'secondary':'#353535'
+      }
+    },
   },
   variants: {},
   plugins: [],
-}
+};
